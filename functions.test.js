556bd457ea3ce7271189356bfd3e6e2ddb265f87
Oliver <olivercarlsonoc@gmail.com>
2019-01-16 08:57:39 -0800

    first commit

---
+++ functions.test.js
@@ -0,0 +1,86 @@
+const functions = require("./functions");
+
+// beforeEach( () => initDatabase());
+// afterEach( () => closeDatabase());
+
+// beforeAll( () => initDatabase());
+// afterAll( () => closeDatabase());
+
+
+// const initDatabase = () => console.log('Database initialized...');
+// const closeDatabase = () => console.log('Database closed...');
+const nameCheck = () => console.log('checking name');
+
+describe('Checking names',  () => {
+    beforeEach(() => nameCheck() );
+
+    test('User is Jefe', () => {
+        const user = 'Jefe';
+        expect(user).toBe('Jefe');
+    })
+
+    test('User is Karen', () => {
+        const user = 'Karen';
+        expect(user).toBe('Karen');
+    })
+
+
+});
+
+
+
+
+//Async Await
+
+test("User fetched name should be Leanne Graham", async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual("Leanne Graham");
+});
+
+
+//Promise
+test("User fetched name should be Leanne Graham", () => {
+    expect.assertions(1);
+    return functions.fetchUser().then(data => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
+});
+
+// test("Adds 2 + 2 to equal 4", () => {
+//   expect(functions.add(2, 2)).toBe(4);
+// });
+
+// test("Adds 2 + 2 to NOT equal 5", () => {
+//   expect(functions.add(2, 2)).not.toBe(5);
+// });
+
+// test("Should be Null", () => {
+//   expect(functions.isNull()).toBeNull();
+// });
+
+// test("Should be falsy", () => {
+//   expect(functions.checkValue(null)).toBeFalsy();
+// });
+
+// test("User should be Joe Blow object", () => {
+//   expect(functions.createUser()).toEqual({
+//     firstName: "Joe",
+//     lastName: "Blow"
+//   });
+// });
+
+// test('Should be under 1600', () => {
+//     const load1 = 800;
+//     const load2 = 700;
+//     expect(load1 + load2).toBeLessThan(1600);
+// });
+
+// test('There is no I in team', () => {
+//     expect('team').not.toMatch(/I/);
+// });
+
+// test('Admin should be in usernames', () => {
+//     usernames = ['john', 'karen', 'admin'];
+//     expect(usernames).toContain('admin');
+// });
